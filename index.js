86aa9933190e2e155ee2276c2fd0ee1d9d916cd0
Ryan Youngdale <youngdale95@gmail.com>
2018-03-04 23:56:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// var array1 = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-// var array2 = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-// var array3 = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function theBeatlesPlay(array1, array2) {
   var arr = [];
   for(var i=0; i<array1.length; i++){
